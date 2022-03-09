e9435efe7ed6083ea25d9fc19b1e75f77420b84b
choral7451 <chorales@naver.com>
2022-03-10 01:59:16 +0900

    백엔드에서 로그인 구현

--- app/app.js
+++ app/app.js
@@ -11,8 +11,11 @@ app.set("views", "./src/views");
 // 템플릿 엔진 세팅
 app.set("view engine", "ejs");
 
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home); // use => 미들웨어 등록 메서드.
 
 module.exports = app;
-
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,8 +18,10 @@ function login()  {
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
-    });
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then(console.log);
 }
 
     
