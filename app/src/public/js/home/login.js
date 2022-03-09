d8ae15bc952c71455580addef83fb2351d6784e5
choral7451 <chorales@naver.com>
2022-03-10 01:28:00 +0900

    DOM을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector('#id'),
+    psword = document.querySelector('#password'),
+    loginBtn = document.querySelector('button')
+
+loginBtn.addEventListener('click', login);
+
+// 에로우함수 사용시 결과가 다르다
+function login()  {
+    const req = {
+        id : id.value,
+        psword : psword.value 
+    };
+}
+
+    
